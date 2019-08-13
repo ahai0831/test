8ff9522ce916df37fd1879aa833eb6ab7eb0bc0a
客户端-王亚涛 <wangyt@corp.21cn.com>
2019-08-13 17:50:08 +0800

    using google-style to format file

--- src/ele-dll/electron-dll-proj.cpp
+++ src/ele-dll/electron-dll-proj.cpp
@@ -1,131 +1,126 @@
-#include <thread>
-#include <chrono>
-#include <tuple>
+ï»¿#include <chrono>
 #include <new>
+#include <thread>
+#include <tuple>
 
 #include <Assistant_v2.h>
 #include <slicedownload_mastercontrol.h>
 #include "electron-dll-proj.h"
 
-struct Assist_Type
-{
-	int64_t nLocalAddr;
-	typedef std::tuple<std::unique_ptr<std::string>, std::unique_ptr<SlicedownloadMastercontrol>> DLMapValue;
-	typedef std::string DLMapKey;
-	typedef std::map<DLMapKey, DLMapValue> DLMap;
-	DLMap downloadInfo;
+struct Assist_Type {
+  int64_t nLocalAddr;
+  typedef std::tuple<std::unique_ptr<std::string>,
+                     std::unique_ptr<SlicedownloadMastercontrol>>
+      DLMapValue;
+  typedef std::string DLMapKey;
+  typedef std::map<DLMapKey, DLMapValue> DLMap;
+  DLMap downloadInfo;
 
-	typedef std::tuple<std::unique_ptr<std::string>, std::string> RegMapValue;
-	typedef std::string RegMapKey;
-	typedef std::map<RegMapKey, RegMapValue> RegMap;
-	RegMap regisStrings;
+  typedef std::tuple<std::unique_ptr<std::string>, std::string> RegMapValue;
+  typedef std::string RegMapKey;
+  typedef std::map<RegMapKey, RegMapValue> RegMap;
+  RegMap regisStrings;
 
-	std::shared_ptr<assistant::Assistant_v2> pAssist;
-	Assist_Type() : nLocalAddr(0LL), pAssist(std::make_shared<assistant::Assistant_v2>())
-	{
-		nLocalAddr = (int64_t)(this);
-	}
-	/// ¸´ÖÆ¹¹Ôì¡¢Ä¬ÈÏ¹¹Ôì¡¢ÒÆ¶¯¹¹Ôì¡¢=ºÅ²Ù×÷·û
-	/// ÒªÃ´½ûÓÃµô£¬ÒªÃ´ÏÔÊ½Ð´Çå
-private:
-	Assist_Type(Assist_Type const &) = delete;
-	Assist_Type &operator=(Assist_Type const &) = delete;
-	Assist_Type(Assist_Type &&) = delete;
+  std::shared_ptr<assistant::Assistant_v2> pAssist;
+  Assist_Type()
+      : nLocalAddr(0LL), pAssist(std::make_shared<assistant::Assistant_v2>()) {
+    nLocalAddr = (int64_t)(this);
+  }
+  /// å¤åˆ¶æž„é€ ã€é»˜è®¤æž„é€ ã€ç§»åŠ¨æž„é€ ã€=å·æ“ä½œç¬¦
+  /// è¦ä¹ˆç¦ç”¨æŽ‰ï¼Œè¦ä¹ˆæ˜¾å¼å†™æ¸…
+ private:
+  Assist_Type(Assist_Type const &) = delete;
+  Assist_Type &operator=(Assist_Type const &) = delete;
+  Assist_Type(Assist_Type &&) = delete;
 };
 
-extern "C" int64_t CreateAst()
-{
-	auto ast_ptr = new (std::nothrow) Assist_Type();
-	return ast_ptr->nLocalAddr;
+extern "C" int64_t CreateAst() {
+  auto ast_ptr = new (std::nothrow) Assist_Type();
+  return ast_ptr->nLocalAddr;
 }
 
-extern "C" char *StartSliceDownload(char *sDownloadURL, char *sDownloadPath, int64_t nAst)
-{
-	auto downloadid_ptr = std::make_unique<std::string>(std::move(assistant::uuid::generate()));
-	char *flag = nullptr;
-	if (nullptr != sDownloadPath && nullptr != sDownloadURL && strlen(sDownloadPath) > 0 && strlen(sDownloadURL) > 0)
-	{
-		auto ast_ptr = (Assist_Type *)nAst;
-		auto downloadctrl_ptr = std::make_unique<SlicedownloadMastercontrol>(ast_ptr->pAssist);
-		flag = const_cast<char *>(downloadid_ptr->data());
+extern "C" char *StartSliceDownload(char *sDownloadURL, char *sDownloadPath,
+                                    int64_t nAst) {
+  auto downloadid_ptr =
+      std::make_unique<std::string>(std::move(assistant::uuid::generate()));
+  char *flag = nullptr;
+  if (nullptr != sDownloadPath && nullptr != sDownloadURL &&
+      strlen(sDownloadPath) > 0 && strlen(sDownloadURL) > 0) {
+    auto ast_ptr = (Assist_Type *)nAst;
+    auto downloadctrl_ptr =
+        std::make_unique<SlicedownloadMastercontrol>(ast_ptr->pAssist);
+    flag = const_cast<char *>(downloadid_ptr->data());
 
-		downloadctrl_ptr->AsyncProcess(sDownloadURL, sDownloadPath);
-		auto piter = ast_ptr->downloadInfo.emplace(std::string(flag), std::make_tuple(std::move(downloadid_ptr), std::move(downloadctrl_ptr)));
-	}
-	return flag;
+    downloadctrl_ptr->AsyncProcess(sDownloadURL, sDownloadPath);
+    auto piter = ast_ptr->downloadInfo.emplace(
+        std::string(flag), std::make_tuple(std::move(downloadid_ptr),
+                                           std::move(downloadctrl_ptr)));
+  }
+  return flag;
 }
 
-extern "C" char *RegisterSliceDownloadSubscription(f4download::OnNext fNext, f4download::OnComplete fComplete, char *sDownloadID, int64_t nAst)
-{
-	char *csRegID = nullptr;
-	if (nullptr != sDownloadID)
-	{
-		auto ptrA_v2 = (Assist_Type *)nAst;
-		auto pIter = ptrA_v2->downloadInfo.find(sDownloadID);
-		if (pIter != ptrA_v2->downloadInfo.end())
-		{
-			auto &downloadCtrl = std::get<1>(pIter->second);
-			auto speedNext = [fNext](uint64_t ns) {
-				auto sspeed = std::to_string(ns);
-				std::string sRaw = R"({"real_speed":")";
-				sRaw += sspeed;
-				sRaw += '\"';
-				sRaw += '}';
-				fNext(sRaw.c_str());
-			};
-			if (nullptr != downloadCtrl)
-			{
-				auto sRegID = downloadCtrl->RegSpeedCallback(speedNext, fComplete);
-				auto reg_ptr = std::make_unique<std::string>(std::move(sRegID));
-				csRegID = const_cast<char *>(reg_ptr->data());
-				ptrA_v2->regisStrings.emplace(sRegID, std::make_tuple(std::move(reg_ptr), sDownloadID));
-			}
-		}
-	}
-	return csRegID;
+extern "C" char *RegisterSliceDownloadSubscription(
+    f4download::OnNext fNext, f4download::OnComplete fComplete,
+    char *sDownloadID, int64_t nAst) {
+  char *csRegID = nullptr;
+  if (nullptr != sDownloadID) {
+    auto ptrA_v2 = (Assist_Type *)nAst;
+    auto pIter = ptrA_v2->downloadInfo.find(sDownloadID);
+    if (pIter != ptrA_v2->downloadInfo.end()) {
+      auto &downloadCtrl = std::get<1>(pIter->second);
+      auto speedNext = [fNext](uint64_t ns) {
+        auto sspeed = std::to_string(ns);
+        std::string sRaw = R"({"real_speed":")";
+        sRaw += sspeed;
+        sRaw += '\"';
+        sRaw += '}';
+        fNext(sRaw.c_str());
+      };
+      if (nullptr != downloadCtrl) {
+        auto sRegID = downloadCtrl->RegSpeedCallback(speedNext, fComplete);
+        auto reg_ptr = std::make_unique<std::string>(std::move(sRegID));
+        csRegID = const_cast<char *>(reg_ptr->data());
+        ptrA_v2->regisStrings.emplace(
+            sRegID, std::make_tuple(std::move(reg_ptr), sDownloadID));
+      }
+    }
+  }
+  return csRegID;
 }
 
-extern "C" bool CancelSubscription(char *sRegisterID, int64_t nAst)
-{
-	bool bSucceed = false;
-	auto ptrA_v2 = (Assist_Type *)nAst;
-	auto pIter = ptrA_v2->regisStrings.find(sRegisterID);
-	if (pIter != ptrA_v2->regisStrings.end())
-	{
-		Assist_Type::RegMapValue &tuple_strings = pIter->second;
-		auto &sDownloadID = std::get<1>(tuple_strings);
-		auto pIter_downloadid = ptrA_v2->downloadInfo.find(sDownloadID);
-		if (pIter_downloadid != ptrA_v2->downloadInfo.end())
-		{
-			auto &downloadCtrl = std::get<1>(pIter_downloadid->second);
-			if (nullptr != downloadCtrl)
-			{
-			}
-		}
-	}
-	return bSucceed;
+extern "C" bool CancelSubscription(char *sRegisterID, int64_t nAst) {
+  bool bSucceed = false;
+  auto ptrA_v2 = (Assist_Type *)nAst;
+  auto pIter = ptrA_v2->regisStrings.find(sRegisterID);
+  if (pIter != ptrA_v2->regisStrings.end()) {
+    Assist_Type::RegMapValue &tuple_strings = pIter->second;
+    auto &sDownloadID = std::get<1>(tuple_strings);
+    auto pIter_downloadid = ptrA_v2->downloadInfo.find(sDownloadID);
+    if (pIter_downloadid != ptrA_v2->downloadInfo.end()) {
+      auto &downloadCtrl = std::get<1>(pIter_downloadid->second);
+      if (nullptr != downloadCtrl) {
+      }
+    }
+  }
+  return bSucceed;
 }
 
-extern "C" void StopDownload(char *sDownloadID, int64_t nAst)
-{
-	auto ptrA_v2 = (Assist_Type *)nAst;
-	auto pIter = ptrA_v2->downloadInfo.find(sDownloadID);
-	if (pIter != ptrA_v2->downloadInfo.end())
-	{
-		auto& downloadCtrl = std::get<1>(pIter->second);
-		auto another_thread = std::thread([&downloadCtrl]() {
-			std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-			downloadCtrl->AsyncStop();
-		});
-		if (another_thread.joinable())
-		{
-			another_thread.join();
-		}
-	}
+extern "C" void StopDownload(char *sDownloadID, int64_t nAst) {
+  auto ptrA_v2 = (Assist_Type *)nAst;
+  auto pIter = ptrA_v2->downloadInfo.find(sDownloadID);
+  if (pIter != ptrA_v2->downloadInfo.end()) {
+    auto &downloadCtrl = std::get<1>(pIter->second);
+    auto another_thread = std::thread([&downloadCtrl]() {
+      std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+      downloadCtrl->AsyncStop();
+    });
+    if (another_thread.joinable()) {
+      another_thread.join();
+    }
+  }
 }
 
-extern "C" void DestroyAst(int64_t nAst)
-{
-	auto ast_ptr = (Assist_Type *)nAst;
-	delete ast_ptr;
+extern "C" void DestroyAst(int64_t nAst) {
+  auto ast_ptr = (Assist_Type *)nAst;
+  delete ast_ptr;
 }
--- src/ele-dll/electron-dll-proj.h
+++ src/ele-dll/electron-dll-proj.h
@@ -1,21 +1,23 @@
 #include <memory>
 
-namespace f4download
-{
+namespace f4download {
 typedef void (*OnNext)(const char *);
 typedef void (*OnComplete)();
-} // namespace f4download
+}  // namespace f4download
 
 //新建Ast对象句柄；<句柄><void>
 extern "C" int64_t CreateAst();
 
 //启动分片下载流程；
 //<下载流程uuid><下载链接，下载路径，Ast句柄>
-extern "C" char *StartSliceDownload(char *sDownloadURL, char *sDownloadPath, int64_t nAst);
+extern "C" char *StartSliceDownload(char *sDownloadURL, char *sDownloadPath,
+                                    int64_t nAst);
 
 //注册下载回调
 //<订阅的uuid><OnNext回调，OnComplete回调，下载流程uuid，Ast句柄>
-extern "C" char *RegisterSliceDownloadSubscription(f4download::OnNext fNext, f4download::OnComplete fComplete, char *sDownloadID, int64_t nAst);
+extern "C" char *RegisterSliceDownloadSubscription(
+    f4download::OnNext fNext, f4download::OnComplete fComplete,
+    char *sDownloadID, int64_t nAst);
 
 //取消回调订阅
 //<返回取消结果><订阅uuid，Ast句柄>
