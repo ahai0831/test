32002a365413f474d83d72291582a38fae88be19
邓健喆 <dengjzh1@corp.21cn.com>
2019-09-04 18:38:05 +0800

    Remove all third_party source packages, and download them from internet. Resolves #15
    Remove zlib source package.
    
    Remove libuv source package;
    Upgrade libuv to 1.20.3(Last version supporting XP);
    Add additional libuv1.31(For >= Win7).
    
    Improve zlib props.
    
    For zlib, use *.tar.xz .
    
    Improve slice download.
    
    Remove openssl source package.
    Add config_openssl_include vcproj, improve build speed.
    
    Improve zlib build speed.
    
    Improve c-ares build speed.
    
    Remove curl source package.
    
    Little improve.
    
    Remove googletest source package.
    
    Remove c-arers and curl source package.
    
    Remove rxcpp and concurrentqueue source package.
    
    Upgrade concurrentqueue.
    
    Improve decompression.bat .

---
+++ vcproj/batch-scripts/package_get.bat
@@ -0,0 +1,45 @@
+@REM this script works with a *.csv file, then download it from source and check it.
+@REM work dir is the dir of *.csv file.
+@echo off
+if \"\"==\"%1\" goto error
+
+setlocal EnableDelayedExpansion
+cd /d "%~dp1"
+for %%a in ("%~1") do set target_csv_file=%%~fa
+echo [info]Solve:%target_csv_file%
+@REM analysis *.csv
+for /f "usebackq skip=1 tokens=1,2,3 delims=," %%i in ("%target_csv_file%") do (
+    @REM download file
+    >nul 2>&1 dir "%%i" ||(
+        echo [info]Download %%i ...************************************************
+        echo [info]Try %%j
+        call "%~dp0aria2c.bat" -s3 -x3 -k3M -c %%j
+        echo [info]************************************************
+    )
+    @REM download file if resume from break point
+    >nul 2>&1 dir "%%i.aria2" &&(
+        echo [info]Download %%i ...************************************************
+        echo [info]Try %%j
+        call "%~dp0aria2c.bat" -s3 -x3 -k3M -c %%j
+        echo [info]************************************************
+    )
+    @REM check file
+    set n=1
+    for /f "usebackq skip=1" %%m in (`certutil -hashfile "%%i" SHA1`) do (
+        if 1==!n! (
+            echo [info]File SHA1:%%m ? SHA1 in csv:%%k
+            if %%m==%%k (
+                echo [Valid_file]%%~fi
+                goto final
+            )
+        )
+        set /a n+=1
+    )
+)
+:error
+endlocal
+exit /b 65535
+
+:final
+endlocal
+exit /b
