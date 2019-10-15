23b3ebf0da779398db8f81818541a971e47bb6f0
tianyou <tiany@corp.21cn.com>
2019-10-15 10:13:08 +0800

    better get disk serial method.resolve #47.
    format.
    
    improve and correct details.resolve #47.
    
    format again.
    
    improve initialization and other details.
    
    add empty line at end of files.
    
    little improve.
    
    format.
    
    format again.
    
    change details.
    
    improve xp virtual machine situation.
    
    improve and format.

--- third_party/cloud_base/src/UniqueMachineCode/UniqueMachineCode.cpp
+++ third_party/cloud_base/src/UniqueMachineCode/UniqueMachineCode.cpp
@@ -1,198 +1,143 @@
 #include "UniqueMachineCode.h"
 
+#include <comdef.h>
+
 #include <cinttypes>
 #include <iostream>
-
-#include <Wbemidl.h>
-#include <comdef.h>
-#include <intrin.h>
+#include <memory>
 
 #include "HashAlgorithm\SHA1Digest.h"
 
-#define SHA1_DIGESTSIZE (20)
-
 namespace {
-// sha1 encode test key
-const char *key = "1234567890ABCDEF";
+typedef struct _ASTAT_ {
+  ADAPTER_STATUS adapt;
+  NAME_BUFFER NameBuff[30];
+} ASTAT;
 }  // namespace
 
 namespace cloud_base {
 namespace unique_machine_code {
+
 // get Mac address
-std::string get_mac_address(void) {
-  char mac[200];
-  NCB ncb;
-  typedef struct _ASTAT_ {
-    ADAPTER_STATUS adapt;
-    NAME_BUFFER NameBuff[30];
-  } ASTAT, *PASTAT;
-
-  ASTAT Adapter;
-
-  typedef struct _LANA_ENUM {
-    UCHAR length;
-    UCHAR lana[MAX_LANA];
-  } LANA_ENUM;
-
-  LANA_ENUM lana_enum;
-  UCHAR uRetCode;
-  memset(&ncb, 0, sizeof(ncb));
-  memset(&lana_enum, 0, sizeof(lana_enum));
-  ncb.ncb_command = NCBENUM;
-  ncb.ncb_buffer = (unsigned char *)&lana_enum;
-  ncb.ncb_length = sizeof(LANA_ENUM);
-  uRetCode = Netbios(&ncb);
-
-  if (uRetCode != NRC_GOODRET) return "";
-
-  for (int lana = 0; (lana < lana_enum.length) && (uRetCode == NRC_GOODRET);
-       lana++) {
-    ncb.ncb_command = NCBRESET;
-    ncb.ncb_lana_num = lana_enum.lana[lana];
+std::string get_mac_address() {
+  char mac[200] = {'\0'};
+  NCB ncb = {0};
+  ASTAT Adapter = {0};
+  LANA_ENUM lana_enum = {0};
+  UCHAR uRetCode = NRC_GOODRET;
+  std::string strMac;
+  do {
+    ncb.ncb_command = NCBENUM;
+    ncb.ncb_buffer = (unsigned char *)&lana_enum;
+    ncb.ncb_length = sizeof(LANA_ENUM);
     uRetCode = Netbios(&ncb);
-  }
 
-  if (uRetCode != NRC_GOODRET) return "";
+    if (uRetCode != NRC_GOODRET) {
+      break;
+    }
 
-  memset(&ncb, 0, sizeof(ncb));
-  ncb.ncb_command = NCBASTAT;
-  ncb.ncb_lana_num = lana_enum.lana[0];
-  strcpy((char *)ncb.ncb_callname, "*");
-  ncb.ncb_buffer = (unsigned char *)&Adapter;
-  ncb.ncb_length = sizeof(Adapter);
-  uRetCode = Netbios(&ncb);
+    for (int lana = 0; lana < lana_enum.length; lana++) {
+      ncb.ncb_command = NCBRESET;
+      ncb.ncb_lana_num = lana_enum.lana[lana];
+      uRetCode = Netbios(&ncb);
+    }
 
-  if (uRetCode != NRC_GOODRET) return "";
+    if (uRetCode != NRC_GOODRET) {
+      break;
+    }
 
-  sprintf(mac, "%02X-%02X-%02X-%02X-%02X-%02X",
-          Adapter.adapt.adapter_address[0], Adapter.adapt.adapter_address[1],
-          Adapter.adapt.adapter_address[2], Adapter.adapt.adapter_address[3],
-          Adapter.adapt.adapter_address[4], Adapter.adapt.adapter_address[5]);
-  std::string strMac = mac;
+    ncb.ncb_command = NCBASTAT;
+    ncb.ncb_lana_num = lana_enum.lana[0];
+    strcpy((char *)ncb.ncb_callname, "*");
+    ncb.ncb_buffer = (unsigned char *)&Adapter;
+    ncb.ncb_length = sizeof(Adapter);
+    uRetCode = Netbios(&ncb);
+
+    if (uRetCode != NRC_GOODRET) {
+      break;
+    }
+
+    sprintf(mac, "%02X-%02X-%02X-%02X-%02X-%02X",
+            Adapter.adapt.adapter_address[0], Adapter.adapt.adapter_address[1],
+            Adapter.adapt.adapter_address[2], Adapter.adapt.adapter_address[3],
+            Adapter.adapt.adapter_address[4], Adapter.adapt.adapter_address[5]);
+    strMac = mac;
+  } while (false);
   return strMac;
 }
 
 // get disk serial
-std::string get_disk_serial(void) {
-  std::string result;
-  HRESULT hres = S_FALSE;
-
-  IWbemLocator *pLoc = nullptr;
-  IWbemServices *pSvc = nullptr;
-  IEnumWbemClassObject *pEnumerator = nullptr;
-  IWbemClassObject *pclsObj = nullptr;
+std::string get_disk_serial() {
+  HANDLE hPhysicalDrive = INVALID_HANDLE_VALUE;
+  ULONG ulSerialLen = 0;
+  char serialNumber[1024] = {'\0'};
+  std::string strSerialNumber;
   /// use do-while(false)
   do {
-    /// Initialize COM
-    hres = CoInitializeEx(NULL, COINIT_MULTITHREADED);
-    if (S_OK != hres) {
+    hPhysicalDrive = CreateFileA(R"(\\.\PhysicalDrive0)", 0,
+                                 FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+                                 OPEN_EXISTING, 0, NULL);
+    if (hPhysicalDrive == INVALID_HANDLE_VALUE) {
       break;
     }
-    /// Obtain the initial locator to WMI
-    CoCreateInstance(CLSID_WbemLocator, 0, CLSCTX_INPROC_SERVER,
-                     IID_IWbemLocator, (LPVOID *)&pLoc);
+    STORAGE_PROPERTY_QUERY query = {static_cast<STORAGE_PROPERTY_ID>(0)};
+    DWORD cbBytesReturned = 0;
+    query.PropertyId = StorageDeviceProperty;
+    query.QueryType = PropertyStandardQuery;
 
-    if (nullptr == pLoc) {
-      break;
-    }
-    /// Connect to WMI through the IWbemLocator::ConnectServer method
-    pLoc->ConnectServer(L"ROOT\\CimV2",  // Object path of WMI namespace
-                        NULL,            // User name. NULL = current user
-                        NULL,            // User password. NULL = current
-                        0,               // Locale. NULL indicates current
-                        WBEM_FLAG_CONNECT_USE_MAX_WAIT,  // Security flags.
-                        0,     // Authority (e.g. Kerberos)
-                        0,     // Context object
-                        &pSvc  // pointer to IWbemServices proxy
-    );
+    STORAGE_DESCRIPTOR_HEADER pDevDescHeader = {0};
+    auto header_result = DeviceIoControl(
+        hPhysicalDrive, IOCTL_STORAGE_QUERY_PROPERTY, &query, sizeof(query),
+        &pDevDescHeader, sizeof(pDevDescHeader), &cbBytesReturned, NULL);
 
-    if (nullptr == pSvc) {
-      break;
-    }
-    /// Set security levels on the proxy
-    auto proxyblanket_result =
-        CoSetProxyBlanket(pSvc,                    // Indicates the proxy to set
-                          RPC_C_AUTHN_WINNT,       // RPC_C_AUTHN_xxx
-                          RPC_C_AUTHZ_NONE,        // RPC_C_AUTHZ_xxx
-                          NULL,                    // Server principal name
-                          RPC_C_AUTHN_LEVEL_CALL,  // RPC_C_AUTHN_LEVEL_xxx
-                          RPC_C_IMP_LEVEL_IMPERSONATE,  // RPC_C_IMP_LEVEL_xxx
-                          NULL,                         // client identity
-                          EOAC_NONE                     // proxy capabilities
-        );
-    if (S_OK != proxyblanket_result) {
+    if (header_result == 0) {
       break;
     }
 
-    /// Use the IWbemServices pointer to make requests of WMI
-    /// Microsoft docs says that IWbemServices::ExecQuery method's
-    /// Minimum supported client is Windows Vista
-    auto exec_query_result =
-        pSvc->ExecQuery(L"WQL", bstr_t("SELECT * FROM Win32_PhysicalMedia"),
-                        WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
-                        NULL, &pEnumerator);
+    auto disk_property_buffer = std::make_unique<char[]>(pDevDescHeader.Size);
 
-    if (S_OK != exec_query_result) {
+    if (nullptr == disk_property_buffer) {
+      break;
+    }
+    void *buffer_ptr = &(disk_property_buffer[0]);
+    auto device_result = DeviceIoControl(
+        hPhysicalDrive, IOCTL_STORAGE_QUERY_PROPERTY, &query, sizeof(query),
+        buffer_ptr, pDevDescHeader.Size, &cbBytesReturned, NULL);
+
+    if (device_result == 0) {
       break;
     }
 
-    /// Get the data from the query
-    if (nullptr == pEnumerator) {
-      break;
-    }
-    /// Microsoft docs says that IEnumWbemClassObject::Next method's
-    /// Minimum supported client is Windows Vista
-    ULONG uReturn = 0;
-    HRESULT next_result =
-        pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
-    if (S_OK != next_result) {
-      break;
-    }
+    STORAGE_DEVICE_DESCRIPTOR &descriptor =
+        *static_cast<STORAGE_DEVICE_DESCRIPTOR *>(buffer_ptr);
 
-    if (0 == uReturn) {
+    if (descriptor.SerialNumberOffset == 0) {
       break;
     }
-
-    /// Get the value of the Name property
-    /// Microsoft docs says that IEnumWbemClassObject::Get method's
-    /// Minimum supported client is Windows Vista
-    VARIANT vtProp;
-    HRESULT get_result = pclsObj->Get(L"SerialNumber", 0, &vtProp, 0, 0);
-    if (SUCCEEDED(get_result) && (V_VT(&vtProp) == VT_BSTR)) {
-      std::wstring wstrDiskNum = vtProp.bstrVal;
-      result.resize(wstrDiskNum.size());
-      for (decltype(wstrDiskNum.size()) i = 0; i < wstrDiskNum.size(); ++i) {
-        result[i] = static_cast<char>(wstrDiskNum[i]);
+    _snscanf(((const char *)&descriptor + descriptor.SerialNumberOffset),
+             (descriptor.Size - descriptor.SerialNumberOffset), "%1023[^\0]",
+             serialNumber);
+    // in windows xp sp3(virtual machine),it may cause serialnumber is "3030..."
+    const auto sn_size = strlen(serialNumber);
+    if (sn_size % 2 == 0) {
+      decltype(strlen("")) i = 0;
+      for (; i < sn_size; i += 2) {
+        if (strncmp(serialNumber + i, "30", 2) != 0) {
+          break;
+        }
+      }
+      if (sn_size == i) {
+        break;
       }
     }
-    /// Cleanup
-    VariantClear(&vtProp);
-
+    strSerialNumber = serialNumber;
   } while (false);
 
-  if (nullptr != pclsObj) {
-    pclsObj->Release();
-    pclsObj = nullptr;
+  if (hPhysicalDrive != INVALID_HANDLE_VALUE) {
+    CloseHandle(hPhysicalDrive);
+    hPhysicalDrive = INVALID_HANDLE_VALUE;
   }
-  if (nullptr != pEnumerator) {
-    pEnumerator->Release();
-    pEnumerator = nullptr;
-  }
-  if (nullptr == pSvc) {
-    pSvc->Release();
-    pSvc = nullptr;
-  }
-  if (nullptr != pLoc) {
-    pLoc->Release();
-    pLoc = nullptr;
-  }
-
-  if (S_FALSE != hres) {
-    // Uninitialize
-    CoUninitialize();
-    hres = S_FALSE;
-  }
-  return result;
+  return strSerialNumber;
 }
 
 // get Cpuid
@@ -200,7 +145,7 @@ void getcpuid(unsigned int CPUInfo[4], unsigned int InfoType) {
 #if defined(__GNUC__)  // GCC
   __cpuid(InfoType, CPUInfo[0], CPUInfo[1], CPUInfo[2], CPUInfo[3]);
 #elif defined(_MSC_VER)  // MSVC
-#if _MSC_VER >= 1400     // VC2005²ÅÖ§³Ö__cpuid
+#if _MSC_VER >= 1400     // VC2005???¡ì??__cpuid
   __cpuid((int *)(void *)CPUInfo, (int)InfoType);
 #else
   getcpuidex(CPUInfo, InfoType, 0);
@@ -209,7 +154,7 @@ void getcpuid(unsigned int CPUInfo[4], unsigned int InfoType) {
 }
 
 // get strCpuId
-std::string get_cpu_id(void) {
+std::string get_cpu_id() {
   std::string strs;
   unsigned int dwBuf[4] = {0};
   getcpuid(dwBuf, 0);
@@ -220,13 +165,16 @@ std::string get_cpu_id(void) {
 }
 
 // GenerateUniqueMachineCode
-std::string generate_unique_machine_code(void) {
+std::string generate_unique_machine_code() {
   std::string strMacAddress = get_mac_address();
   std::string strDiskNum = get_disk_serial();
   std::string strCpuId = get_cpu_id();
   std::string reData = "MacAddress=" + strMacAddress + "&CpuId=" + strCpuId +
                        "&DiskNum=" + strDiskNum;  // UniqueMachineCode
-  std::string enResult = hash_algorithm::GenerateSha1Digest(key, reData);
+  const char key[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9',
+                      '0', 'A', 'B', 'C', 'D', 'E', 'F', '\0'};
+  std::string strKey = key;
+  std::string enResult = hash_algorithm::GenerateSha1Digest(strKey, reData);
   return enResult;
 }
 
