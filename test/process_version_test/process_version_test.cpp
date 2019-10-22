85515a2ee457c5bfc6dde3b44711ae985f2efc31
zhouqp <zhouqp@corp.21cn.com>
2019-10-22 17:11:24 +0800

    resolve #56

---
+++ test/process_version_test/process_version_test.cpp
@@ -0,0 +1,18 @@
+﻿/**
+ * @brief process_version test.
+ * @date 2019-10-18
+ *
+ * @copyright Copyright (c) 2019
+ *
+ */
+#include <iostream>
+#include <string>
+
+#include "process_version\process_version.h"
+int main() {
+  std::string proVersion =
+      cloud_base::process_version::GetCurrentProcessVersion();
+  std::cout << "process version:" << proVersion << std::endl;
+  return 0;
+}
+
