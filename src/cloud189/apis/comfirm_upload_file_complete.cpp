97180fbfb28005b0555177d009804905154b4ff5
tianyou <tiany@corp.21cn.com>
2019-10-29 17:56:04 +0800

    version 1.0.resolve #67.
    format.
    
    version 1.1.
    
    format.
    
    version 1.2.
    
    version 1.3.

---
+++ src/cloud189/apis/comfirm_upload_file_complete.cpp
@@ -0,0 +1,106 @@
+﻿#include "comfirm_upload_file_complete.h"
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <json/json.h>
+
+#include <tools/string_format.hpp>
+
+#include "restful_common/jsoncpp_helper/jsoncpp_helper.hpp"
+
+namespace {
+// 这些是请求中一些固定的参数
+const static char* host = "https://api.cloud.189.cn";
+const static char* uri = "/dciClientCommit.action";
+const static char* method = "POST";
+const static int flag = 1;
+const static int resume_policy = 1;
+const static char* client_type = "TELEPC";
+const static char* channel_id = "web_cloud.189.cn";
+
+}  // namespace
+
+namespace Cloud189 {
+namespace Apis {
+namespace ComfirmUploadFileComplete {
+
+// 用于构建一个json字符串，包含创建文件上传需要的参数
+std::string JsonStringHelper(const std::string& fileCommitUrl,
+                             const int64_t uploadFileId, const int32_t opertype,
+                             const int32_t isLog) {
+  bool is_ok = false;
+  std::string json_str;
+  do {
+    if (fileCommitUrl.empty()) {
+      break;
+    }
+    json_str = assistant::tools::string::StringFormat(
+        "{\"fileCommitUrl\" :%s,\"uploadFileId\" : %" PRId64
+        ",\"opertype\" : %d,\"isLog\" : %d}",
+        fileCommitUrl.c_str(), uploadFileId, opertype, isLog);
+    is_ok = true;
+  } while (false);
+
+  if (is_ok) {
+    return json_str;
+  } else {
+    return "";
+  }
+}
+
+bool HttpRequestEncode(const std::string& params_json,
+                       assistant::HttpRequest& request) {
+  bool is_ok = false;
+  do {
+    // parse json
+    static Json::CharReaderBuilder char_reader_builder;
+    std::unique_ptr<Json::CharReader> char_reader(
+        char_reader_builder.newCharReader());
+    if (nullptr == char_reader) {
+      break;
+    }
+    Json::Value json_str;
+    if (false == char_reader->parse(params_json.c_str(),
+                                    params_json.c_str() + params_json.length(),
+                                    &json_str, nullptr)) {
+      break;
+    }
+    std::string fileCommitUrl =
+        restful_common::jsoncpp_helper::GetString(json_str["fileCommitUrl"]);
+    int64_t uploadFileId =
+        restful_common::jsoncpp_helper::GetInt64(json_str["uploadFileId"]);
+    int32_t opertype =
+        restful_common::jsoncpp_helper::GetInt(json_str["opertype"]);
+    int32_t isLog = restful_common::jsoncpp_helper::GetInt(json_str["isLog"]);
+
+    // TODO(tiany): GET version
+
+    // TODO(tiany): GET rand
+
+    request.url = std::string(host) + std::string(uri);
+    request.method = method;
+
+    // TODO(tiany): set header(use SessionHelper)
+
+    // TODO(tiany): set url(params)
+
+    // TODO(tiany): set body
+
+    is_ok = true;
+  } while (false);
+
+  return is_ok;
+}
+
+bool HttpResponseDecode(const assistant::HttpResponse& response,
+                        const assistant::HttpRequest& request,
+                        std::string& response_info) {
+  // 请求的响应暂时不用有具体定义，先这样写
+  return false;
+}
+
+}  // namespace ComfirmUploadFileComplete
+}  // namespace Apis
+}  // namespace Cloud189
---
+++ src/cloud189/apis/comfirm_upload_file_complete.h
@@ -0,0 +1,89 @@
+﻿/**
+ * @file comfirm_upload_file_complete.h
+ * @date 2019-10-29
+ * @copyright Copyright (c) 2019
+ *
+ */
+#pragma once
+#ifndef COMFIRM_UPLOAD_FILE_COMPLETE_H
+#define COMFIRM_UPLOAD_FILE_COMPLETE_H
+
+#include <cinttypes>
+
+#include <http_primitives.h>
+
+// 天翼云的顶层命名空间（包括个人云和家庭云）
+namespace Cloud189 {
+namespace Apis {
+namespace ComfirmUploadFileComplete {
+
+// json字符串包含的字段应于这个函数声明完全一致
+// fileCommitUrl,[string],
+// [确认文件上传完成URL]
+// uploadFileId,[string],
+// [表明断点续传文件Id]
+// opertype, [int32_t],
+// [表明上传后操作方式,
+// 1-遇到相同文件名(只检查文件名)，执行重命名操作。
+// 3-遇到相同文件名（只检查文件名），执行覆盖原文件]
+// isLog, [int32_t],
+// [客户端日志上传标识，
+// 1–客户端日志文件上传至指定账户
+// 0-非客户端日志文件上传]
+
+std::string JsonStringHelper(const std::string& fileCommitUrl,
+                             const int64_t uploadFileId, const int32_t opertype,
+                             const int32_t isLog);
+
+// jsoncpp parse 原地解析
+// jsoncpp reader 严格模式
+//
+// 请求方式：POST
+// header规定:Content—Type: application/x-www-form-urlencoded
+//
+// 需要放到url中的参数：
+//
+// clientType, [string],
+// [json字符串不用传，参数放到url中, 表明客户端类型，PC端值固定位TELEPC]
+// version, [string],
+// [json字符串不用传, 参数放到url中, 表明客户端版本，调用ProcessVersion方法获取]
+// channelId, [string],
+// [json字符串不用传, 参数放到url中, 表明渠道id，值固定为web_cloud.189.cn]
+// rand, [string]
+// [json字符串不用传, 参数放到url中,
+// 随机数，现固定为4位随机数加下划线加8位随机数（1234_12345678）]
+//
+// 需要放到header中的参数：
+//
+// Date, [string], SessionKey, [string], Signature, [string],
+// [这三个参数json字符串不用传，调用SessionHelper直接原子地设置这三个参数]
+// X-Request-ID, [string],
+// [json字符串不用传, 参数放到header中，调用assitant的UUID模块获取]
+//
+// 需要放到body中的参数：
+//
+// uploadFileId,[int64_t],
+// [从json字符串中作为int64_t解析，参数放到body中，表明上传文件Id]
+// opertype, [int32_t],
+// [上传后操作方式
+// 1-遇到相同文件名(只检查文件名)，执行重命名操作
+// 3-遇到相同文件名（只检查文件名），执行覆盖原文件
+// isLog, [int32_t],
+// [从json字符串中作为int32_t解析，参数放到body中，客户端日志上传标识，
+// 1–客户端日志文件上传至指定账户
+// 0-非客户端日志文件上传]
+// ResumePolicy, [int32_t],
+// [json字符串不用传，表明是否支持断点续传控制，1表示支持,0表示不支持]
+
+bool HttpRequestEncode(const std::string& params_json,
+                       assistant::HttpRequest& request);
+
+bool HttpResponseDecode(const assistant::HttpResponse& response,
+                        const assistant::HttpRequest& request,
+                        std::string& response_info);
+
+}  // namespace ComfirmUploadFileComplete
+}  // namespace Apis
+}  // namespace Cloud189
+
+#endif  // COMFIRM_UPLOAD_FILE_COMPLETE_H
