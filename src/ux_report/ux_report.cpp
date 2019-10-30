618fac9f15916e390891dba71c07a10031017b04
tianyou <tiany@corp.21cn.com>
2019-10-30 18:18:39 +0800

    version 1.0.resolve #74.
    format.
    
    verison 1.1.
    
    version 1.2.
    
    .
    
    version 1.3.

---
+++ src/ux_report/ux_report.cpp
@@ -0,0 +1,32 @@
+﻿#include "ux_report.h"
+
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <cinttypes>
+
+#include <json/json.h>
+
+#include <tools/string_format.hpp>
+
+#include "restful_common/jsoncpp_helper/jsoncpp_helper.hpp"
+
+namespace UxReport {
+
+bool HttpRequestEncode(const std::string& params_json,
+                       assistant::HttpRequest& request) {
+
+  // TODO(tiany): base64
+
+  // TODO(tiany): get host
+
+  // TODO(tiany): get uri
+
+  // TODO(tiany): set request header and body
+
+  //返回 request
+}
+
+}  // namespace UxReport
---
+++ src/ux_report/ux_report.h
@@ -0,0 +1,37 @@
+﻿/**
+ * @file ux_report.h
+ * @date 2019-10-30
+ * @copyright Copyright (c) 2019
+ */
+#pragma once
+#ifndef UX_REPORT_H
+#define UX_REPORT_H
+
+#include <cinttypes>
+
+#include <http_primitives.h>
+
+// 云涛上报请求命名空间
+namespace UxReport {
+// json字符串包含的字段应于这个函数声明完全一致
+// data, [string],
+// [表明要上传的日志数据（加密后的）]
+
+std::string JsonStringHelper(const std::string& data);
+
+// 请求方式：POST
+// header规定：Content—Type: application/x-www-form-urlencoded
+// SdkVersion, [string],
+// [表明PC云涛SDK的版本号，参数放到header中,调用ProcessVersion方法获取]
+// OsType, [string],
+// [表明系统类型，参数放到header中,PC端值为5]
+// TODO(tiany):host and uri
+// X-Request-ID, [string],
+// [json字符串不用传, 参数放到header中，调用assitant的UUID模块获取]
+// data, [string],
+// [从json字符串中作为string解析,表明要上传的日志数据（加密后的）]
+
+bool HttpRequestEncode(const std::string& params_json,
+                       assistant::HttpRequest& request);
+}  // namespace UxReport
+#endif  // UX_REPORT_H
