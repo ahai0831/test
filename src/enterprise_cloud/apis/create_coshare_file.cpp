e370a4e8c810c203b127b8f33e8d7e73d7c6eac2
tianyou <tiany@corp.21cn.com>
2019-11-04 10:55:57 +0800

    version 1.0. resolve #80.
    version 1.1.
    
    version 1.3.

---
+++ src/enterprise_cloud/apis/create_coshare_file.cpp
@@ -0,0 +1,42 @@
+﻿#include "create_coshare_file.h"
+
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <cinttypes>
+
+#include <json/json.h>
+
+#include <tools/string_format.hpp>
+
+#include "restful_common/jsoncpp_helper/jsoncpp_helper.hpp"
+
+namespace EnterpriseCloud {
+namespace Apis {
+namespace CreateCoshareFile {
+
+// 用于构建一个json字符串，包含创建文件上传需要的参数
+std::string JsonStringHelper(const int64_t corpId, const int64_t coshareId,
+                             const std::string& parentId,
+                             const std::string& fileName) {
+  std::string json_str = "";
+  return json_str;
+}
+
+bool HttpRequestEncode(const std::string& params_json,
+                       assistant::HttpRequest& request) {
+  return false;
+}
+
+bool HttpResponseDecode(const assistant::HttpResponse& response,
+                        const assistant::HttpRequest& request,
+                        std::string& response_info) {
+  // 请求的响应暂时不用有具体定义，先这样写
+  return false;
+}
+
+}  // namespace CreateCoshareFile
+}  // namespace Apis
+}  // namespace EnterpriseCloud
