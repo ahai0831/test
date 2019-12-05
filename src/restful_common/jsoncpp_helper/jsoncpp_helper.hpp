829b4d5948cfc66c2a169bcc20aa44d050d6bdc0
tianyou <tiany@corp.21cn.com>
2019-12-05 12:24:33 +0800

    add reader helper and writer helper.

--- src/restful_common/jsoncpp_helper/jsoncpp_helper.hpp
+++ src/restful_common/jsoncpp_helper/jsoncpp_helper.hpp
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * @brief restful_common::jsoncpp_helper .hpp
  * @date 2019-10-22
  *
@@ -9,8 +9,9 @@
 #ifndef JSONCPP_HELPER_JSONCPP_HELPER_H__
 #define JSONCPP_HELPER_JSONCPP_HELPER_H__
 #include <cinttypes>
+#include <memory>
 
-#include <json\json.h>
+#include <json/json.h>
 namespace restful_common {
 namespace jsoncpp_helper {
 // get string
@@ -46,6 +47,45 @@ inline uint32_t GetUint(const Json::Value &value) {
 // get double
 inline double GetDouble(const Json::Value &value) {
   return value.isDouble() ? value.asDouble() : 0;
+}
+
+// 规范读取，使用严格模式且原地解析
+inline bool ReaderHelper(const std::string &json_info,
+                         Json::Value &json_value) {
+  bool flag = false;
+  do {
+    if (json_info.empty()) {
+      break;
+    }
+    Json::CharReaderBuilder reader_builder;
+    Json::CharReaderBuilder::strictMode(&reader_builder.settings_);
+    std::unique_ptr<Json::CharReader> const reader_ptr(
+        reader_builder.newCharReader());
+    if (nullptr == reader_ptr) {
+      break;
+    }
+    if (!reader_ptr->parse(json_info.c_str(),
+                           json_info.c_str() + json_info.size(), &json_value,
+                           nullptr)) {
+      break;
+    }
+    flag = true;
+  } while (false);
+  return flag;
+}
+
+// 规范写入，无格式
+inline std::string WriterHelper(const Json::Value &json_value) {
+  std::string json_info;
+  Json::StreamWriterBuilder wbuilder;
+  wbuilder.settings_["indentation"] = "";
+  json_info = Json::writeString(wbuilder, json_value);
+  return json_info;
+}
+
+inline void WriterHelper(const Json::Value &json_value,
+                         std::string &json_info) {
+  json_info = WriterHelper(json_value);
 }
 
 }  // namespace jsoncpp_helper
