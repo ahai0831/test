18e28a4a3e16c87c914e465be32f2673b7478c2b
tianyou <tiany@corp.21cn.com>
2020-01-04 10:47:23 +0800

    add third party gn support.to be improve.tiany.

--- test/hello_gn/hello_gn.cpp
+++ test/hello_gn/hello_gn.cpp
@@ -1,11 +1,25 @@
+#include <cinttypes>
 #include <cstdio>
+#include <memory>
 
 #include <zlib.h>
 
 #include <openssl/crypto.h>
 #include <openssl/ssl.h>
 
+#include <Assistant_v3.hpp>
+
 int main(void) {
   printf("Hello GN.\n");
+
+  std::shared_ptr<assistant::Assistant_v3> assist =
+      std::make_shared<assistant::Assistant_v3>();
+
+  assistant::HttpRequest req_get(
+      "https://postman-echo.com/get?foo1=bar1&foo2=bar2");
+  auto res_get = assist->SyncHttpRequest(req_get);
+  printf("InPile: %d--------%s--------Body:\n%s\n\n", res_get.status_code,
+         res_get.effective_url.c_str(), res_get.body.c_str());
+
   return 0;
 }
